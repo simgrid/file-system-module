4f84e0a3f55cfcfef1f3d8dff56d858e0942e426
Henri Casanova <henric@hawaii.edu>
2024-05-02 16:12:34 -1000

    Updated documentation generation

--- conf/cmake/Documentation.cmake
+++ conf/cmake/Documentation.cmake
@@ -11,7 +11,7 @@ if (DOXYGEN_FOUND)
     # FSMOD APIs documentation
     foreach (SECTION USER)
         string(TOLOWER ${SECTION} SECTION_LOWER)
-        set(DOXYGEN_OUT ${CMAKE_HOME_DIRECTORY}/docs/logs/Doxyfile_${SECTION})
+	set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/docs/logs/Doxyfile_${SECTION})
         set(FSMOD_DOC_INPUT "${CMAKE_HOME_DIRECTORY}/src ${CMAKE_HOME_DIRECTORY}/include")
 
         if (${SECTION} STREQUAL "USER")
@@ -24,17 +24,20 @@ if (DOXYGEN_FOUND)
         configure_file(${CMAKE_HOME_DIRECTORY}/conf/doxygen/Doxyfile.in ${DOXYGEN_OUT} @ONLY)
 
         add_custom_target(doc-${SECTION_LOWER}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                WORKING_DIRECTORY ${CMAKE_HOME_DIRECTORY}
                 COMMENT "Generating FSMOD ${SECTION} documentation" VERBATIM)
         add_custom_command(TARGET doc-${SECTION_LOWER}
-                COMMAND mkdir -p ${CMAKE_HOME_DIRECTORY}/docs/${FSMOD_RELEASE_VERSION}/${SECTION_LOWER}
-                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT})
+		COMMAND echo "HERE"
+		COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/docs/${FSMOD_RELEASE_VERSION}/${SECTION_LOWER}
+		COMMAND echo "CALLING DOXYGEN: ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}"
+                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+		COMMAND echo "CALLING DOX")
 
         LIST(APPEND FSMOD_SECTIONS_LIST doc-${SECTION_LOWER})
     endforeach ()
 
     # FSMOD documentation pages
-    set(DOXYGEN_OUT ${CMAKE_HOME_DIRECTORY}/docs/logs/Doxyfile_pages)
+    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/docs/logs/Doxyfile_pages)
     set(FSMOD_DOC_INPUT "${CMAKE_HOME_DIRECTORY}/doc ${CMAKE_HOME_DIRECTORY}/src ${CMAKE_HOME_DIRECTORY}/include")
     set(FSMOD_SECTIONS "USER")
     set(FSMOD_SECTIONS_OUTPUT "pages")
@@ -42,31 +45,26 @@ if (DOXYGEN_FOUND)
     configure_file(${CMAKE_HOME_DIRECTORY}/conf/doxygen/Doxyfile.in ${DOXYGEN_OUT} @ONLY)
 
     get_directory_property(extra_clean_files ADDITIONAL_MAKE_CLEAN_FILES)
-    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${extra_clean_files};${CMAKE_HOME_DIRECTORY}/docs")
+    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${extra_clean_files};${CMAKE_CURRENT_BINARY_DIR}/docs")
 
     add_custom_target(doc DEPENDS fsmod ${FSMOD_SECTIONS_LIST})
 
     add_custom_command(TARGET doc
-            COMMAND rm -rf ${CMAKE_HOME_DIRECTORY}/docs/source
-            COMMAND cp -r ${CMAKE_HOME_DIRECTORY}/doc/source ${CMAKE_HOME_DIRECTORY}/docs)
-    add_custom_command(TARGET doc COMMAND python3
-                        ${CMAKE_HOME_DIRECTORY}/doc/scripts/generate_rst.py 
-                        ${CMAKE_HOME_DIRECTORY}/docs/${FSMOD_RELEASE_VERSION}
-                        ${CMAKE_HOME_DIRECTORY}/docs/source
-                        ${FSMOD_RELEASE_VERSION})
-    add_custom_command(TARGET doc COMMAND sphinx-build 
-                        ${CMAKE_HOME_DIRECTORY}/docs/source
-                        ${CMAKE_HOME_DIRECTORY}/docs/build/${FSMOD_RELEASE_VERSION})
-    add_custom_command(TARGET doc COMMAND cp -R
-                        ${CMAKE_HOME_DIRECTORY}/docs/build/${FSMOD_RELEASE_VERSION}
-                        ${CMAKE_HOME_DIRECTORY}/docs/build/latest)
+	    COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/docs/source
+	    COMMAND cp -r ${CMAKE_HOME_DIRECTORY}/doc/source ${CMAKE_CURRENT_BINARY_DIR}/docs)
     add_custom_command(TARGET doc 
-	    COMMAND rm -rf ${CMAKE_HOME_DIRECTORY}/docs/${FSMOD_RELEASE_VERSION}
-	    COMMAND rm -rf ${CMAKE_HOME_DIRECTORY}/docs/latest
-	    COMMAND rm -rf ${CMAKE_HOME_DIRECTORY}/docs/logs
-	    COMMAND rm -rf ${CMAKE_HOME_DIRECTORY}/docs/source
-	    COMMAND mv ${CMAKE_HOME_DIRECTORY}/docs/build/* ${CMAKE_HOME_DIRECTORY}/docs
-	    COMMAND rmdir ${CMAKE_HOME_DIRECTORY}/docs/build)
+	    COMMAND python3 ${CMAKE_HOME_DIRECTORY}/doc/scripts/generate_rst.py ${CMAKE_CURRENT_BINARY_DIR}/docs/${FSMOD_RELEASE_VERSION} ${CMAKE_CURRENT_BINARY_DIR}/docs/source ${FSMOD_RELEASE_VERSION})
+    add_custom_command(TARGET doc 
+	    COMMAND sphinx-build ${CMAKE_CURRENT_BINARY_DIR}/docs/source ${CMAKE_CURRENT_BINARY_DIR}/docs/build/${FSMOD_RELEASE_VERSION})
+    add_custom_command(TARGET doc 
+	    COMMAND cp -R ${CMAKE_CURRENT_BINARY_DIR}/docs/build/${FSMOD_RELEASE_VERSION} ${CMAKE_CURRENT_BINARY_DIR}/docs/build/latest)
+    add_custom_command(TARGET doc 
+	    COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/docs/${FSMOD_RELEASE_VERSION}
+	    COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/docs/latest
+	    COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/docs/logs
+	    COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/docs/source
+	    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/docs/build/* ${CMAKE_CURRENT_BINARY_DIR}/docs
+	    COMMAND rmdir ${CMAKE_CURRENT_BINARY_DIR}/docs/build)
 
 
 endif()
