a84ff4fbcf5c27f475c0573a3632b7df18e39232
Henri Casanova <henric@hawaii.edu>
2024-05-16 15:21:38 -1000

    More caching test

--- test/caching_test.cpp
+++ test/caching_test.cpp
@@ -95,6 +95,55 @@ TEST_F(CachingTest, FIFODontEvictOpenFiles) {
     });
 }
 
+
+TEST_F(CachingTest, FIFOExtensive)  {
+    DO_TEST_WITH_FORK([this]() {
+        this->setup_platform();
+        // Create one actor (for this test we could likely do it all in the maestro but what the hell)
+        sg4::Actor::create("TestActor", host_, [this]() {
+
+            std::vector<std::tuple<std::vector<std::string>, std::vector<std::string>, std::vector<std::string>>> experiments =
+                    {
+                            {{"create", "f1", "20MB"}, {"f1"}, {}},
+                            {{"create", "f2", "50MB"}, {"f1", "f2"}, {}},
+                            {{"create", "f3", "30MB"}, {"f1", "f2", "f3"}, {}},
+                            {{"delete", "f1"},         {"f2", "f3"}, {"f1"}},
+                            {{"create", "f4", "30MB"}, {"f3", "f4"}, {"f2"}},
+                            {{"create", "f5", "50MB"}, {"f4", "f5"}, {"f3"}},
+                            {{"create", "f6", "10MB"}, {"f4", "f5", "f6"}, {}},
+                            {{"create", "f7", "10MB"}, {"f4", "f5", "f6", "f7"}, {}},
+                            {{"create", "f8", "10MB"}, {"f5", "f6", "f7", "f8"}, {"f4"}},
+                    };
+
+            for (auto const &xp : experiments) {
+                // Perform action
+                auto action = std::get<0>(xp);
+                auto expected_files_there = std::get<1>(xp);
+                auto expected_files_not_there = std::get<2>(xp);
+                if (action.at(0) == "create") {
+                    auto file_name = action.at(1);
+                    auto file_size = action.at(2);
+                    this->fs_->create_file("/dev/fifo/" + file_name, file_size);
+                } else if (action.at(0) == "delete") {
+                    auto file_name = action.at(1);
+                    this->fs_->unlink_file("/dev/fifo/" + file_name);
+                }
+                // Check state
+                for (auto const &f : expected_files_there) {
+                    ASSERT_TRUE(this->fs_->file_exists("/dev/fifo/" + f));
+                }
+                for (auto const &f : expected_files_not_there) {
+                    ASSERT_FALSE(this->fs_->file_exists("/dev/fifo/" + f));
+                }
+            }
+
+        });
+
+        // Run the simulation
+        ASSERT_NO_THROW(sg4::Engine::get_instance()->run());
+    });
+}
+
 TEST_F(CachingTest, LRUBasics)  {
     DO_TEST_WITH_FORK([this]() {
         this->setup_platform();
@@ -114,6 +163,62 @@ TEST_F(CachingTest, LRUBasics)  {
             ASSERT_TRUE(this->fs_->file_exists("/dev/lru/20mb.txt"));
             ASSERT_FALSE(this->fs_->file_exists("/dev/lru/60mb.txt"));
             ASSERT_TRUE(this->fs_->file_exists("/dev/lru/30mb.txt"));
+        });
+
+        // Run the simulation
+        ASSERT_NO_THROW(sg4::Engine::get_instance()->run());
+    });
+}
+
+
+
+TEST_F(CachingTest, LRUExtensive)  {
+    DO_TEST_WITH_FORK([this]() {
+        this->setup_platform();
+        // Create one actor (for this test we could likely do it all in the maestro but what the hell)
+        sg4::Actor::create("TestActor", host_, [this]() {
+
+            std::vector<std::tuple<std::vector<std::string>, std::vector<std::string>, std::vector<std::string>>> experiments =
+                    {
+                            {{"create", "f1", "20MB"}, {"f1"}, {}},
+                            {{"create", "f2", "50MB"}, {"f1", "f2"}, {}},
+                            {{"create", "f3", "30MB"}, {"f1", "f2", "f3"}, {}},
+                            {{"access", "f1"},         {"f1", "f2", "f3"}, {}},
+                            {{"create", "f4", "30MB"}, {"f1", "f3", "f4"}, {"f2"}},
+                            {{"delete", "f1"},         {"f3", "f4"}, {"f1"}},
+                            {{"create", "f5", "10MB"}, {"f3", "f4", "f5"}, {}},
+                            {{"create", "f6", "80MB"}, {"f5", "f6"}, {"f3", "f4"}},
+                            {{"access", "f5"},         {"f5", "f6"}, {}},
+                            {{"create", "f7", "20MB"}, {"f5", "f7"}, {"f6"}},
+                    };
+
+            for (auto const &xp : experiments) {
+                // Perform action
+                auto action = std::get<0>(xp);
+                auto expected_files_there = std::get<1>(xp);
+                auto expected_files_not_there = std::get<2>(xp);
+                if (action.at(0) == "create") {
+                    auto file_name = action.at(1);
+                    auto file_size = action.at(2);
+                    this->fs_->create_file("/dev/lru/" + file_name, file_size);
+                } else if (action.at(0) == "delete") {
+                    auto file_name = action.at(1);
+                    this->fs_->unlink_file("/dev/lru/" + file_name);
+                } else if (action.at(0) == "access") {
+                    auto file_name = action.at(1);
+                    auto file = this->fs_->open("/dev/lru/" + file_name);
+                    file->read(1);
+                    file->close();
+                }
+                // Check state
+                for (auto const &f : expected_files_there) {
+                    ASSERT_TRUE(this->fs_->file_exists("/dev/lru/" + f));
+                }
+                for (auto const &f : expected_files_not_there) {
+                    ASSERT_FALSE(this->fs_->file_exists("/dev/lru/" + f));
+                }
+            }
+
         });
 
         // Run the simulation
